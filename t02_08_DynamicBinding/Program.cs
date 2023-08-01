a760ceb5e39b2532ae01bd8352a018ef496d51ae
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-01 12:55:07 +0200

    DynamicBinding

---
+++ t02_08_DynamicBinding/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace t02_08_DynamicBinding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Reflection
+            object obj = "Jugoslav";
+            //obj.GetHashCode();
+
+            var methodInfo = obj.GetType().GetMethod("GetHashCode");
+            methodInfo.Invoke(null, null);
+
+            // Dynamics
+            dynamic excellObject = "Jugoslav";
+            excellObject.Optimize();
+
+            dynamic name = "Jugoslav";
+            name = 10;
+
+            dynamic a = 10;
+            dynamic b = 5;
+            var c = a + b;
+
+            int i = 5;
+            dynamic d = i;
+            long l = d;
+        }
+    }
+}
